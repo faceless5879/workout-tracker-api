f75d16e99b05810acf6ee6c9032a7c989918e64b
Dat.PM <faceless5879@gmai.com>
2022-10-25 19:18:24 +0900

    feat: adding verifyToken to WorkRoutes and ExerciseRoutes

--- Controllers/Exercise/ExerciseController.js
+++ Controllers/Exercise/ExerciseController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 const { Pool, Client } = require("pg");
+const { ERROR_MSGS } = require("../../Configs/Constants");
+
 const pool = new Pool();
 const client = new Client({
   database: process.env.PGDATABASE,
@@ -17,13 +19,13 @@ const ExerciseController = {
       console.log(test.rows);
       console.log(exerciseid);
       if (exerciseid === undefined) {
-        res.status(500).json({ message: "Internal Server Error" });
+        res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
         return;
       }
       res.status(200).json({ message: "success" });
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: "Internal Server Error" });
+      res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
     }
   },
 };
--- Routes/Exercise/ExerciseRoutes.js
+++ Routes/Exercise/ExerciseRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const exercise_ctrl = require("../../Controllers/Exercise/ExerciseController");
+const { verifyToken } = require("../../Controllers/validationMiddleware");
+
+router.use("/", verifyToken, (req, res, next) => {
+  next();
+});
 router.get("/:exerciseid", exercise_ctrl.getExercise);
 module.exports = router;
