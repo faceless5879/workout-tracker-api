a4984e090f1734beefba7806e0b676b1ec3847ae
Dat.PM <faceless5879@gmai.com>
2022-10-25 21:57:08 +0900

    fix: add config

--- Controllers/Exercise/ExerciseController.js
+++ Controllers/Exercise/ExerciseController.js
@@ -1,22 +1,11 @@
 const express = require("express");
 require("dotenv").config();
-const { Pool, Client } = require("pg");
 const { ERROR_MSGS } = require("../../Configs/Constants");
 
-const pool = new Pool();
-const client = new Client({
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-});
-client.connect();
 const ExerciseController = {
   getExercise: async (req, res) => {
     try {
       const { exerciseid } = req.params;
-      const test = await client.query("SELECT * FROM checkins LIMIT 10");
-      await client.end();
-      console.log(test.rows);
       console.log(exerciseid);
       if (exerciseid === undefined) {
         res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
