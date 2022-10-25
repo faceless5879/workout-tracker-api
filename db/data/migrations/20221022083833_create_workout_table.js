6c811280ec455e4f1f21f6191924c6fad5c0fab7
Yamaki09 <kojie09yamashita@gmail.com>
2022-10-25 21:28:35 +0900

    feat: workout and exercise data

--- db/data/migrations/20221022083833_create_workout_table.js
+++ db/data/migrations/20221022083833_create_workout_table.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
 	return knex.schema.createTable("workout", function (table) {
 		table.increments("id").unsigned().primary();
 		table.string("name", 255);
-		table.integer("day_of_week", 7);
+		table.integer("day_of_week", 7).checkBetween([0, 6]);
 		table.integer("user_id").references("id").inTable("user");
 	});
 };
